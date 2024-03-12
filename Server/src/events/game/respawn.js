c7a2218b6e8024e7324932cfed3db35268a8ff2a
= <=>
2024-03-11 22:46:35 -0400

    fix respawn event

--- Server/src/events/game/respawn.js
+++ Server/src/events/game/respawn.js
@@ -1,7 +1,11 @@
 module.exports = {
     match: "RespawnManager has successfully spawned",
     run(line) {
-        return { name: 'respawn', context: line };
+        const data = {
+            players: line.split('RespawnManager has successfully spawned')[1].split('players as')[0].trim(),
+            class:   line.split('players as')[1].split('!')[0].trim()
+        };
+        return { name: 'respawn', context: data };
     }
     // RespawnManager has successfully spawned 1 players as NineTailedFox!
 }
