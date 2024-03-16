c67f884990642a1ca63d75b51a979b2e87cb4b12
= <=>
2024-03-15 21:04:09 -0400

    fix kill event firing as teamkill

--- Server/src/events/game/kill.js
+++ Server/src/events/game/kill.js
@@ -13,7 +13,7 @@ module.exports = {
                 class: line.split(',')[2].split('playing as:')[1].split('.')[0].trim()
             }
         };
-        return { name: 'teamkill', context: data };
+        return { name: 'kill', context: data };
     }
     // Front (front@northwood), playing as Scientist, has been killed by Artificial_ (76561198167038756@steam) playing as: Class-D Personnel. Specific death reason: Shot by Artificial_ with GunFRMG0 to the 'Headshot' hitbox.
 }
